c79320f02f92d611cb296b4c67260f7ab8377e6f
ZoogerZager <joeymarchese@gmail.com>
2016-02-15 16:00:44 -0500

    Added the GPIO Code
    Ready to test with the board.

--- morseled.py
+++ morseled.py
@@ -10,8 +10,9 @@ Rules of Morse Code from https://en.wikipedia.org/wiki/Morse_code:
 5. The space between words is seven units.
 
 """
-# import RPi.GPIO as GPIO
-# from time import sleep
+import RPi.GPIO as GPIO
+from time import sleep
+
 
 def morse_translate(input_text):
     """translates the input text into morse skipping special characters."""
@@ -24,31 +25,51 @@ def morse_translate(input_text):
                 translated_text.append(morse_dict[letter])
     except(KeyError):
         pass
-    return translated_text    
-
-# GPIO setup code
-# LEDPin = 22
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(LEDPin, GPIO.OUT)
+    return translated_text
     
-
+# GPIO setup code
+LEDPin = 22
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LEDPin, GPIO.OUT)
+    
 # Initialize the Morse Dictionary and get the key, value pairs.
 morse_dict = {}
 with open ('morse_dictionary.txt', 'r') as data:
     for line in data:
         letter = line.rsplit()
         morse_dict[letter[0]] = letter[1]
-        
-# Test Code for the Morse Dictionary
-# for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789':
-    # print(letter, morse_dict[letter])
 
 # Get the user's input and clean it.    
 user_input = input('Enter a string: ')
-# This isn't working with special chars in the middle of strings...
+# This isn't working with special chars in the middle of letters
 user_input = user_input.strip('!@#$%^&*()')
 user_input = user_input.upper()
-print(user_input)
 
-print(morse_translate(user_input))
+# Print the cleaned and translated user_input
+print(user_input)
+morse_code = morse_translate(user_input)
+print(morse_code)
+
+# Blinking Logic and Output
+try:
+    for letter in morse_code:
+        if letter == 'space':
+            sleep(7) # Time between words.
+        else:
+            for blip in letter:
+                if blip == '.':
+                    GPIO.output(LEDPin, True)
+                    sleep(1) # Length of Dot
+                    GPIO.output(LEDPin, False)
+                if blip == '-':
+                    GPIO.ouput(LEDPin, True)
+                    sleep(3) # Length of Dash
+                    GPIO.output(LEDPin, False)
+                sleep(1) # Time between each blip of the same letter.    
+            sleep(3) # Time between letters of the same word.
+finally:
+    GPIO.cleanup()
+                    
+                 
+            
 
