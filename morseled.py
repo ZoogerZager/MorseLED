6cccf6528322b05c60046040e37c69fdbc41181a
ZoogerZager <joeymarchese@gmail.com>
2016-02-19 10:27:30 -0500

    Save Changes From 2-18
    Adding some more configuration options for testing without the GPIO
    setup.

--- morseled.py
+++ morseled.py
@@ -15,7 +15,7 @@ from time import sleep
 
 
 def morse_translate(input_text):
-    """translates the input text into morse skipping special characters."""
+    """translates the input_text into morsecode skipping special characters."""
     translated_text = []
     try:
         for letter in input_text:
@@ -27,10 +27,45 @@ def morse_translate(input_text):
         pass
     return translated_text
     
-# GPIO setup code
-LEDPin = 22
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LEDPin, GPIO.OUT)
+def GPIO_output():
+    """Handles the default GPIO Blinking Logic and Output"""
+    try:
+        for letter in morse_code:
+            if letter == 'space':
+                sleep(space_time) # Time between words.
+            else:
+                sleep(dash_time) # Time between letters of the same word.
+                for blip in letter:
+                    sleep(dot_time) # Time between each blip of the same letter. 
+                    if blip == '.':
+                        GPIO.output(LEDPin, True)
+                        sleep(dot_time) # Length of Dot
+                        GPIO.output(LEDPin, False)
+                    if blip == '-':
+                        GPIO.output(LEDPin, True)
+                        sleep(dash_time) # Length of Dash
+                        GPIO.output(LEDPin, False)
+    finally:
+        GPIO.cleanup()
+        
+def Console_output():
+    """Outputs the Morse code as dots and dashes in the Console"""
+            for letter in morse_code:
+            if letter == 'space':
+                sleep(space_time) # Time between words.
+            else:
+                sleep(dash_time) # Time between letters of the same word.
+                for blip in letter:
+                    sleep(dot_time) # Time between each blip of the same letter. 
+                    if blip == '.':
+                        GPIO.output(LEDPin, True)
+                        sleep(dot_time) # Length of Dot
+                        GPIO.output(LEDPin, False)
+                    if blip == '-':
+                        GPIO.output(LEDPin, True)
+                        sleep(dash_time) # Length of Dash
+                        GPIO.output(LEDPin, False)
+    
     
 # Initialize the Morse Dictionary and get the key, value pairs.
 morse_dict = {}
@@ -38,6 +73,20 @@ with open ('morse_dictionary.txt', 'r') as data:
     for line in data:
         character = line.rsplit()
         morse_dict[character[0]] = character[1]
+
+        
+## Configuration
+output_GPIO = True
+output_Console = True
+dot_time = .25 # Customize output speed
+dash_time = dot_time * 3
+space_time = dot_time * 7
+
+# Run GPIO setup.
+if output_GPIO: 
+    LEDPin = 22
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LEDPin, GPIO.OUT)  
 
 # Get the user's input, clean it, and uppercase it.    
 user_input = input('Enter a string: ')
@@ -49,27 +98,11 @@ print(user_input)
 morse_code = morse_translate(user_input)
 print(morse_code)
 
-# Customize the speed of the output in seconds. WARNING: only edit dot_time
-dot_time = .25
-dash_time = dot_time * 3
-space_time = dot_time * 7
+
+if output_GPIO:
+    GPIO_output()
+if output_Console:
+    Console_output()
+
 
 # Blinking Logic and Output
-try:
-    for letter in morse_code:
-        if letter == 'space':
-            sleep(space_time) # Time between words.
-        else:
-            sleep(dash_time) # Time between letters of the same word.
-            for blip in letter:
-                sleep(dot_time) # Time between each blip of the same letter. 
-                if blip == '.':
-                    GPIO.output(LEDPin, True)
-                    sleep(dot_time) # Length of Dot
-                    GPIO.output(LEDPin, False)
-                if blip == '-':
-                    GPIO.output(LEDPin, True)
-                    sleep(dash_time) # Length of Dash
-                    GPIO.output(LEDPin, False)
-finally:
-    GPIO.cleanup()
